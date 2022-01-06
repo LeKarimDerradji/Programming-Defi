4817ad3afd40f32c1d480e8c65a8f67a4bb34819
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-01-06 17:04:45 +0100

    remove_deployer

--- test/Exchange-test.js
+++ test/Exchange-test.js
@@ -5,21 +5,21 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Exchange', function () {
-  let deployer, user1, user2, Token, token, Exchange, exchange;
+  let user1, user2, Token, token, Exchange, exchange;
 
   beforeEach(async function () {
-    [deployer, user1, user2] = await ethers.getSigners();
+    [user1, user2] = await ethers.getSigners();
 
     Token = await ethers.getContractFactory('Token');
-    token = await Token.connect(deployer).deploy('Token', 'TKN', ethers.utils.parseEther('1000000'));
+    token = await Token.deploy('Token', 'TKN', ethers.utils.parseEther('1000000'));
     await token.deployed();
 
     const tokenAddress = token.address;
     Exchange = await ethers.getContractFactory('Exchange');
     exchange = await Exchange.deploy(tokenAddress);
     await exchange.deployed();
-    await token.connect(deployer).transfer(user1.address, ethers.utils.parseEther("100"));
-    await token.connect(deployer).transfer(user2.address, ethers.utils.parseEther("100"));
+    await token.transfer(user1.address, ethers.utils.parseEther("100"));
+    await token.transfer(user2.address, ethers.utils.parseEther("100"));
   });
   it("Should return the new greeting once it's changed", async function () {
     
